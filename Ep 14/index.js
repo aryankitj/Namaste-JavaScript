68f4bad0073793de9ac12b9e0dff6c40c8e1f3c2
Ankit K. <jaisankit1645@gmail.com>
2025-01-25 19:36:22 +0530

    `Added content to Ep 16/index.js file`

--- Ep 14/index.js
+++ Ep 14/index.js
@@ -1,1 +1,29 @@
- gfycy
+//& Callback FUnction
+//fn which is passed as an argument
+setTimeout(function () {
+    console.log("timer");
+}, 5000);
+function x(y) {
+console.log("x");
+y();
+}
+x(function y() {
+console.log("y");
+});
+// x y timer
+// setTimeout is async fn
+
+document.getElementById("clickMe").addEventListener("click", function xyz() {
+  //when event click occurs, this callback function (xyz) is called into callstack
+  console.log("Button clicked");
+});
+
+//Counting Button clicked using closure
+function attachEventList() {
+    //creating new function for closure
+    let count = 0;
+    document.getElementById("clickMe").addEventListener("click", function xyz() {
+        console.log("Button clicked", ++count); //now callback function forms closure with outer scope(count)
+    });
+}
+attachEventList();
---
+++ Ep 15/index.js
@@ -0,0 +1,19 @@
+//& Web API
+// web api is a set of rules and protocols for building and interacting with software applications over the web.
+    // setTimeout
+    // DOM APIs
+    // fetch()
+    // local Storage
+    // console
+    // location
+
+//& Callback Queue
+// Callback Queue is a data structure that holds functions waiting to be executed. eg, setTimeout,  event listeners
+
+//& Event loop
+// Event loop takes the first function from the callback queue  to the empty call stack and executes it.
+
+//& Microtask Queue
+// Microtask Queue is a data structure that holds functions waiting to be executed. eg, Promise.th, MutationObserver, async.
+
+// First Call Stack Empties, then Microtask Queue then Callback Queue.
